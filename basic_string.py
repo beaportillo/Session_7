e641f261b1dbc6a804d6e95e235dfb04be28ffc3
bea <bea.p.timothy@gmail.com>
2026-02-03 17:15:38 +0100

    string examples

---
+++ basic_string.py
@@ -0,0 +1,15 @@
+s1 = "Hello World"
+s2 = "Hello World"
+print(s1)
+print(s2)
+s1 = "yug%(i88" #You can put weird characters
+print(s1)
+
+# We can index a string
+s = "banana"
+print(s[1]) # indexing starts from zero
+print(s[0]) # first letter (b)
+print([10//2])
+
+# You can also index by negatives!
+print(s[-2])
---
+++ string_operators.py
@@ -0,0 +1,6 @@
+# + will perform string concatenation
+s1 = "hello"
+s2 = "bye"
+print(s1 + s2)
+print(s2 + s1)
+print(s1+", "+s2+"!!")
